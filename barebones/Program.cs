83daaf731a68d91738dda1b494b181b4c31b2cac
Jake Bladt <jake@jakebladt.com>
2019-01-31 15:40:00 -0500

    A bare-bones, static files only web server

---
+++ barebones/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+
+namespace barebones
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel(options => options.Listen(IPAddress.Loopback, 8888))
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .Build();
+        }
+
+    }
+}
---
+++ barebones/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace barebones
+{
+    public class Startup : IStartup
+    {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            return services.BuildServiceProvider();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot"))
+            });
+        }
+    }
+}
